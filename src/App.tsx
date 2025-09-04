f20417fc2d03f253eeebb725ac13082ed99955c2
Matthieu Cornut <mc@alba-it.ch>
2025-09-04 13:55:25 +0200

    Deployment setup fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename="/presences">
           <Routes>
               <Route path="/" element={<Presences />} />
               <Route path="/edit/:eleveId" element={<EditEleve />} />
